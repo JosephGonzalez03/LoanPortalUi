27bcbe7bb00a4e907d69d94545d0b9b21e706fdf
JosephGonzalez03 <josephdg3@gmail.com>
2021-01-17 11:11:39 -0800

    feat: Add LoansForm UI

---
+++ src/LoansForm/LoanFormEntry.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class LoanFormEntry extends React.Component {
+    render() {
+        return (
+            <tr>
+                <td><input type="text" name="name" value={this.props.loan.name} /></td>
+                <td><input type="number" name="interestRate" value={this.props.loan.interestRate} /></td>
+                <td><input type="number" name="outstandingBalance" value={this.props.loan.outstandingBalance} /></td>
+                <td><input type="number" name="contribution" value={this.props.loan.contribution} /></td>
+                <td><input type="submit" name="delete" value="-"/></td>
+            </tr>
+        );
+    }
+}
+
+export default LoanFormEntry;
---
+++ src/LoansForm/LoansForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import LoanTableHeader from "../shared/LoanTableHeader";
+import LoanFormEntry from "./LoanFormEntry";
+
+class LoansForm extends React.Component {
+    render() {
+        const data = [
+            {id: 1, name: 'Loan a', interestRate: 5.000, outstandingBalance: 1000.00, contribution: 100.00},
+            {id: 2, name: 'Loan b', interestRate: 5.000, outstandingBalance: 1000.00, contribution: 100.00}];
+        const mLoanEntries = [];
+        data.forEach((loan) => {
+                mLoanEntries.push(
+                    <LoanFormEntry key={loan.id} loan={loan}/>
+                );
+            }
+        );
+        return (
+            <div>
+                <form onSubmit={() => {alert("Submitting...")}}>
+                    <table>
+                        <LoanTableHeader/>
+                        <tbody>{mLoanEntries}</tbody>
+                    </table>
+                    <input type="button" name="add" value="+" />
+                    <input type="submit" name="submit" value="Submit"/>
+                    <input type="reset" name="reset" value="Reset"/>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default LoansForm;
