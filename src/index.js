04521e01754a5c01d23df8cbc29d1ddbe978a219
JosephGonzalez03 <josephdg3@gmail.com>
2021-01-27 14:37:19 -0800

    feat: Add Home.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import LoanTable from './Home/LoanTable';
+import Home from "./Home/Home";
 
 ReactDOM.render(
-  <LoanTable />,
+  <Home />,
   document.getElementById('root')
 );
 
