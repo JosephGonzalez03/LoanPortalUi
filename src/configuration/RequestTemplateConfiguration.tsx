0d265f06913ef7ee2558fac76d3d01e93dda2ba1
JosephGonzalez03 <josephdg3@gmail.com>
2021-09-05 11:18:29 -0700

    feat: Add application.json and ReqeustTemplateConfiguration.tsx

---
+++ src/configuration/RequestTemplateConfiguration.tsx
@@ -0,0 +1,28 @@
+import configuraton from "../resources/applicaton.json";
+
+const {Url, SearchParams} = require('url');
+
+type HttpProperties = {
+    protocol: string;
+    host: string;
+    port: string;
+    basePath: string;
+}
+
+export function apiRequestTemplate(httpProperties: HttpProperties, uri: string, params?: Record<string, string>): Promise<Response> {
+    let url = new Url (httpProperties.protocol + 
+                    "://" + 
+                    httpProperties.host + 
+                    ":" + 
+                    httpProperties.port +
+                    httpProperties.basePath);
+
+    url.search = new URLSearchParams(params).toString();
+    console.log(url)
+    return fetch(url);
+}
+
+export function loanSystemApiProperties(): HttpProperties {
+   let loanSystemApi: HttpProperties = configuraton.providers["loan-system-api"]["mock"];
+   return loanSystemApi;
+}
