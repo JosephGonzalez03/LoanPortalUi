d30627e92f3311605e948b143730fe891304e486
JosephGonzalez03 <josephdg3@gmail.com>
2021-09-08 18:09:17 -0700

    feat: Add loan system api request & response contract types
    Loan system api request & response contract types are required to separate ui logic from api requests / responses.

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,39 +1,72 @@
 import React from "react";
-import {Loan, PaymentSummary, State, LoanAction} from "./components/types/types";
+import {Loan, LoanRequestContract, LoanResponseContract, PaymentSummary, State, LoanAction} from "./components/types/types";
 import {LoanTable} from "./components/LoanTable";
 import {PaymentSummaryTable} from "./components/PaymentSummaryTable";
 import {EditLoansForm} from "./components/forms/EditLoansForm";
+import axios, {AxiosTransformer} from "axios";
 import {loanSystemApi, paymentProcessApi} from "../../configuration/RequestTemplateConfiguration";
 
 const initialState: State = {loans: []};
 export const LoanContext = React.createContext(initialState);
 
+function loanContractRequestTransformation(data: Loan, headers: object): LoanRequestContract {
+   return {
+       name: data.name,
+       interestRate: data.interestRate,
+       outstandingBalance: data.outstandingBalance,
+       contribution: data.contribution
+   }
+}
+        
+function loansContractResponseTrasnsformation(data: LoanResponseContract[]): Loan[] {
+    let loans: Loan[] = data.map(loan => ({
+            id: loan.id,
+            name: loan.name,
+            interestRate: loan.interestRate,
+            outstandingBalance: loan.outstandingBalance,
+            contribution: loan.contribution,
+            isEdited: false,
+            isNew: false
+        })
+    );
+    
+    return loans;
+}
+
 export function HomePage(): JSX.Element {
     const [loans, setLoans] = React.useState<Loan[]>([]);
     const [paymentSummaries, setPaymentSummaries] = React.useState<PaymentSummary[]>([]);
-
+    const [showPaymentSummaryTable, setIsPaymentSummaryTableShown] = React.useState<boolean>(false);
+    
     React.useEffect(() => {
         // get loans from loan-system-api
-        let params: object = {
-            orderBy: 'NAME'
-        };
+        let options = {
+            params: {
+                orderBy: 'NAME'
+            },
+            transformResponse: [...axios.defaults.transformResponse as AxiosTransformer[], loansContractResponseTrasnsformation]
+         };
 
         let userId = '1';
 
-        loanSystemApi.get(`/users/${userId}/loans`, params).then(response => {
+        loanSystemApi.get(`/users/${userId}/loans`, options).then(response => {
             setLoans(response.data);
             dispatch({type: "INIT", loans: response.data});
         });
     }, [])
 
     React.useEffect(() => {
-        let params: object = {
-            operation: 'FORECAST',
-            orderBy: 'NAME'
+        let options = {
+            params: {
+                operation: 'FORECAST',
+                orderBy: 'NAME'
+            }
         }
+
         let userId = '1'
-        paymentProcessApi.get(`/users/${userId}/paymentSummaries`).then(response => setPaymentSummaries(response.data));
-    }, [loans]);
+
+        paymentProcessApi.get(`/users/${userId}/paymentSummaries`, options).then(response => setPaymentSummaries(response.data));
+    }, [showPaymentSummaryTable]);
 
     function buttonClickReducer(state: State, action: LoanAction): State {
         let currentLoans = state.loans;
@@ -47,7 +80,7 @@ export function HomePage(): JSX.Element {
             case "ADD":
                 let oldHighestId: number = loanCopy.sort((loanA, loanB) => loanB.id - loanA.id)[0].id;
 
-                modifiedLoans.push({id: oldHighestId+1, name: "", interestRate: 0.000, outstandingBalance: 0.00, contribution: 0.00});
+                modifiedLoans.push({id: oldHighestId+1, name: "", interestRate: 0.000, outstandingBalance: 0.00, contribution: 0.00, isEdited: false, isNew: true});
                 break;
             case "REMOVE":
                 modifiedLoans = currentLoans.filter(loan => loan.id !== action.loanId);
--- src/pages/home/components/types/types.ts
+++ src/pages/home/components/types/types.ts
@@ -6,6 +6,23 @@ export type Loan = {
     interestRate: number;
     outstandingBalance: number;
     contribution: number;
+    isEdited: boolean;
+    isNew: boolean;
+}
+
+export type LoanRequestContract = {
+    name: string;
+    interestRate: number;
+    outstandingBalance: number;
+    contribution: number;
+}
+
+export type LoanResponseContract = {
+    id: number;
+    name: string;
+    interestRate: number;
+    outstandingBalance: number;
+    contribution: number;
 }
 
 export type PaymentSummary = {
