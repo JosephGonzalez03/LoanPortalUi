90f69a8c9772c42ad37ff181d3e35492a9747e1e
JosephGonzalez03 <josephdg3@gmail.com>
2021-10-31 15:57:54 -0700

    refactor: Move loanFormReducer out of LoanProvider

--- src/util/providers/LoanContextProvider.tsx
+++ src/util/providers/LoanContextProvider.tsx
@@ -46,73 +46,73 @@ const loadInitialLoans = () => {
     return JSON.parse(storedLoans);
 }
 
+function loanFormReducer(loans: Loan[], action: LoanAction): Loan[] {
+    let currentLoans = loans;
+    let modifiedLoans: Loan[] = [...currentLoans];
+
+    switch (action.type) {
+    case "INIT":
+        localStorage.setItem("initialLoans", JSON.stringify(action.loans));
+        modifiedLoans = [...action.loans];
+        break;
+    case "RESET":
+        modifiedLoans = [...loadInitialLoans()];
+        break;
+    case "ADD":
+        let oldHighestId: number = [...currentLoans].sort((loanA, loanB) => loanB.id - loanA.id)[0].id;
+
+        modifiedLoans.push({id: oldHighestId+1, name: "", interestRate: 0.000, outstandingBalance: 0.00, contribution: 0.00, isEdited: false, isNew: true});
+        break;
+    case "REMOVE":
+        modifiedLoans = currentLoans.filter(loan => loan.id !== action.loanId);
+        break;
+    case "EDIT":
+        const updatedValue: string | number = action.event.target.value;
+
+        // update loan object key with input updated value
+        modifiedLoans.forEach((loan) => {
+            if (loan.id === action.loanId) {
+                switch (action.event.target.name) {
+                    case "name":
+                        loan.name = updatedValue;
+                        break;
+                    case "interestRate":
+                        loan.interestRate = Number(updatedValue);
+                        break;
+                    case "outstandingBalance":
+                        loan.outstandingBalance = Number(updatedValue);
+                        break;
+                    case "contribution":
+                        loan.contribution = Number(updatedValue);
+                        break;
+                }
+                loan.isEdited = loan.isNew? false : true;
+            }
+        });
+        break;
+    case "SUBMIT":
+        action.event.preventDefault();
+
+        let userId = 1;
+
+        modifiedLoans.forEach(loan => {
+
+            if (loan.isEdited) {
+                updateLoan(userId, loan.id);
+            }
+
+            if (loan.isNew) {
+                createLoan(userId);
+            }
+        });
+        break;
+    }
+
+    return modifiedLoans;
+}
+
 export default function LoanProvider({children}: {children: React.ReactNode}): JSX.Element {
     const [loans, dispatchLoansAction] = React.useReducer(loanFormReducer, []);
-
-    function loanFormReducer(loans: Loan[], action: LoanAction): Loan[] {
-        let currentLoans = loans;
-        let modifiedLoans: Loan[] = [...currentLoans];
-
-        switch (action.type) {
-        case "INIT":
-            localStorage.setItem("initialLoans", JSON.stringify(action.loans));
-            modifiedLoans = [...action.loans];
-            break;
-        case "RESET":
-            modifiedLoans = [...loadInitialLoans()];
-            break;
-        case "ADD":
-            let oldHighestId: number = [...currentLoans].sort((loanA, loanB) => loanB.id - loanA.id)[0].id;
-
-            modifiedLoans.push({id: oldHighestId+1, name: "", interestRate: 0.000, outstandingBalance: 0.00, contribution: 0.00, isEdited: false, isNew: true});
-            break;
-        case "REMOVE":
-            modifiedLoans = currentLoans.filter(loan => loan.id !== action.loanId);
-            break;
-        case "EDIT":
-            const updatedValue: string | number = action.event.target.value;
-
-            // update loan object key with input updated value
-            modifiedLoans.forEach((loan) => {
-                if (loan.id === action.loanId) {
-                    switch (action.event.target.name) {
-                        case "name":
-                            loan.name = updatedValue;
-                            break;
-                        case "interestRate":
-                            loan.interestRate = Number(updatedValue);
-                            break;
-                        case "outstandingBalance":
-                            loan.outstandingBalance = Number(updatedValue);
-                            break;
-                        case "contribution":
-                            loan.contribution = Number(updatedValue);
-                            break;
-                    }
-                    loan.isEdited = loan.isNew? false : true;
-                }
-            });
-            break;
-        case "SUBMIT":
-            action.event.preventDefault();
-
-            let userId = 1;
-
-            modifiedLoans.forEach(loan => {
-
-                if (loan.isEdited) {
-                    updateLoan(userId, loan.id);
-                }
-
-                if (loan.isNew) {
-                    createLoan(userId);
-                }
-            });
-            break;
-        }
-
-        return modifiedLoans;
-    }
 
     return (
         <LoanContext.Provider value={{loans, dispatchLoansAction}}>
