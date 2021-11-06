0eec36f7b4e15def81753d6cc676d5522dd0e8b1
JosephGonzalez03 <josephdg3@gmail.com>
2021-11-06 13:51:13 -0700

    refactor: Extract State to type

--- src/util/providers/LoanContextProvider.tsx
+++ src/util/providers/LoanContextProvider.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Loan } from "../../api/services/LoanService";
 import loanFormReducer, { LoanAction } from "../reducers/LoanFormReducer";
 
-const initialState: {loans: Loan[], dispatchLoansAction: (action: LoanAction) => void} = {loans: [], dispatchLoansAction: f => f};
+type State = {
+    loans: Loan[],
+    dispatchLoansAction: (action: LoanAction) => void
+}
+
+const initialState: State  = {
+    loans: [],
+    dispatchLoansAction: f => f
+}
+
 export const LoanContext = React.createContext(initialState);
 
 export const useLoans = () => React.useContext(LoanContext);
